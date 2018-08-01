cd606b8711b168dfc6808ce09ada03fe07fde3bf
johnqu1982@gmail.com <johnqu1982@gmail.com>
2018-08-01 14:21:06 +0800

    Refactor home page view to use a template.

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 # class SmokeTest(TestCase):
 #
@@ -11,14 +12,31 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    # These two test method were replaced by one with two lines.
+    #
+    # def test_root_url_resolves_to_home_page_view(self):
+    #     found = resolve('/')
+    #     self.assertEqual(found.func, home_page)
+    #
+    # def test_home_page_returns_correct_html(self):
+    #     # Replace these two lines with one line.
+    #     # request = HttpRequest()
+    #     # response = home_page(request)
+    #     response = self.client.get('/')
+    #     #
+    #     html = response.content.decode('utf8')
+    #     # print(repr(html)) # to check what charactors are actual in it.
+    #     self.assertTrue(html.startswith('<!DOCTYPE html>\n<html'))
+    #     self.assertIn('<title>To-Do lists</title>', html)
+    #     self.assertTrue(html.strip().endswith('</html>'))
+    #     #
+    #     # Replace these two lines with one line.
+    #     # expected_html = render_to_string('home.html')
+    #     # self.assertEqual(html, expected_html)
+    #     self.assertTemplateUsed(response, 'home.html')
+    #     # self.assertTemplateUsed(response, 'wrong.html') # for error messages
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
