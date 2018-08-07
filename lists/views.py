8e3dfbcab267e15caa45dceb9a35c1299c1eb873
johnqu1982@gmail.com <johnqu1982@gmail.com>
2018-08-07 16:29:06 +0800

    Clean up the view.py and simplify the FT.

--- lists/views.py
+++ lists/views.py
@@ -11,10 +11,10 @@ def home_page(request):
     # return render(request, 'home.html', {
     #     'new_item_text': request.POST.get('item_text', ''),
     # })
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
+    # if request.method == 'POST':
+    #     new_item_text = request.POST['item_text']
+    #     Item.objects.create(text=new_item_text)
+    #     return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 
@@ -22,3 +22,8 @@ def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
 
+
+def new_list(request):
+    new_item_text = request.POST['item_text']
+    Item.objects.create(text=new_item_text)
+    return redirect('/lists/the-only-list-in-the-world/')
