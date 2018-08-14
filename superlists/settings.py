2ad4a9a135531a0ec2d4cda1b6df56e045379441
johnqu1982@gmail.com <johnqu1982@gmail.com>
2018-08-14 15:30:03 +0800

    Set johnquicker.pythonanywhere.com into ALLOWED_HOST.

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's0lcdj#l7q70o-l@+(a&pem0v1asnxehh#4^v31zwvt*jzipl-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['johnquicker.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
